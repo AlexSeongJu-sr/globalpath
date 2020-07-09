c76bafa131fefda727bcb1174141d97aaf991176
seongju <djflstkddk@gmail.com>
2020-07-10 03:01:03 +0900

    find_local fixed

--- params.py
+++ params.py
@@ -6,11 +6,12 @@ def get_parameters():
     parser = argparse.ArgumentParser(description = '3D scene graph generation', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--dist_from_obstacle', default = 20, help = '1 = 5cm')
-    parser.add_argument('--origin', default = (-4.34, -3.85), help = 'origin coordinates in map.yaml')
-    parser.add_argument('--slam_map', default = './slam_map/map_0708_2.pgm', help = 'map directory')
+    parser.add_argument('--origin', default = (-4.63, -3.68), help = 'origin coordinates in map.yaml')
+    parser.add_argument('--slam_map', default = './slam_map/map_0709.pgm', help = 'map directory')
     parser.add_argument('--object_category', default = ['tv', 'teddy bear', 'chair'], help = 'object category to look for')
     parser.add_argument('--crop_size', default = np.array([[1.5], [2.0], [2.0]]), help = 'crop_box size(x, y, z)' )
-    parser.add_argument('--cut_distance', default = 1.8, help = 'object farther than this would be ignored. meter unit')
+    parser.add_argument('--cut_distance', default = 1.7, help = 'object farther than this would be ignored. meter unit')
     parser.add_argument('--save_dir', default = './res/', help = 'result files are saved')
+    parser.add_argument('--local_limit', default = 14, help = 'If the distance between far_point & curr_point is under this limit, local path will set curr_point as center. Otherwise, obj_pose will be center.')
 
     return parser.parse_args()
